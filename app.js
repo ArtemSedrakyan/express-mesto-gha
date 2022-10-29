261c1c959971bb54972df53753f162b82a6c5013
Artem Sedrakyan <a.a.sedrakyan@gmail.com>
2022-10-29 20:17:03 +0500

    feature: Добавить cors обработчик

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const router = require('./routes/index');
 const error = require('./middlewares/error');
+const corsMiddleware = require('./middlewares/cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(cors());
+app.use(corsMiddleware);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
