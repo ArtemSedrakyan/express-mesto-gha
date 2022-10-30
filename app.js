8ee9c8d300255568826f3affc68f77691def88ff
Artem Sedrakyan <a.a.sedrakyan@gmail.com>
2022-10-30 22:22:12 +0500

    hotfix: cors fix

--- app.js
+++ app.js
@@ -7,13 +7,37 @@ const { errors } = require('celebrate');
 const router = require('./routes/index');
 const error = require('./middlewares/error');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const corsMiddleware = require('./middlewares/cors');
 
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
 
 const app = express();
-app.use(corsMiddleware);
+app.use((req, res, next) => {
+  const allowedCors = [
+    'https://sedrakyan.mesto.nomoredomains.icu',
+    'http://sedrakyan.mesto.nomoredomains.icu',
+    'https://localhost:3000',
+    'http://localhost:3000',
+  ];
+
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  return next();
+});
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(bodyParser.json());
