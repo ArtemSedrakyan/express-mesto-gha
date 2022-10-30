e416d2d21cb874c4d42ec797fff57cded03687cd
Artem Sedrakyan <a.a.sedrakyan@gmail.com>
2022-10-30 21:09:02 +0500

    hotfix: cors fix

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const router = require('./routes/index');
 const error = require('./middlewares/error');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const corsMiddleware = require('./middlewares/cors');
 
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
 
 const app = express();
-app.use(cors());
+app.use(corsMiddleware);
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(bodyParser.json());
@@ -21,6 +21,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 
 app.use(router);
 
