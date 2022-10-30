9ec4672632a16ccf2e74e01b3a45dfad38aba354
Artem Sedrakyan <a.a.sedrakyan@gmail.com>
2022-10-30 22:52:29 +0500

    hotfix: auth fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,12 +4,14 @@ const {
 } = require('../utils/constants');
 const NotAuthorizedError = require('../errors/NotAuthorizedError');
 
+const { NODE_ENV, JWT_SECRET} = process.env;
+
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
   let payload;
 
   try {
-    payload = jwt.verify(token, 'jsonwebtoken');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     throw new NotAuthorizedError(UNAUTHORIZED_USER_MESSAGE);
   }
