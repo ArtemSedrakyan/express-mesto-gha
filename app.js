edb1879befeccd0d589f3df845cbe264c902b24f
Artem Sedrakyan <a.a.sedrakyan@gmail.com>
2022-10-30 19:05:39 +0500

    hotfix: cors fix

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const router = require('./routes/index');
 const error = require('./middlewares/error');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const corsMiddleware = require('./middlewares/cors');
 
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
 
 const app = express();
-app.use(corsMiddleware());
+app.use(cors());
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(bodyParser.json());
