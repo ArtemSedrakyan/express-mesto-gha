a983a4bfbb23b17da1e3272b97e47ab0ef89bdde
Artem Sedrakyan <a.a.sedrakyan@gmail.com>
2022-11-02 10:49:55 +0500

    hotfix: cors fix

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const router = require('./routes/index');
 const error = require('./middlewares/error');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const corsMiddleware = require('./middlewares/cors');
 
-// Слушаем 3001 порт
-const { PORT = 3001 } = process.env;
+// Слушаем 3000 порт
+const { PORT = 3000 } = process.env;
 
 const app = express();
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-app.use(corsMiddleware);
+app.use(cors({
+  origin: [
+    'https://sedrakyan.mesto.nomoredomains.icu',
+    'http://sedrakyan.mesto.nomoredomains.icu',
+    'https://localhost:3000',
+    'http://localhost:3000',
+  ],
+  credentials: true,
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
